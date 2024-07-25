6abfa1fc8f8e4656afd4b075ae9b9a6a1b2a6efa
mj <justji@dadora.kr>
2024-07-25 20:36:29 +0900

    Revert "5"
    This reverts commit e0c948ac6043ee3ee1d0af4e6efb00542db7fbb3.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import theLittlePrince from './Book/the_little_prince.txt'; // 텍스트 파일�
 //음
 //1
 //2
-//리버트4
-//5
 
 // 타이핑 게임에 사용할 단어 리스트
 const words = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew'];
